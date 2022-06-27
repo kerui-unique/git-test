dd74a5c362b43504d523fc983a69b15ae418caa7
zkr <zkr@atguigu.com>
2022-06-27 15:08:09 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * *
+ *
+ * @create :2022-06-27  14:53
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
